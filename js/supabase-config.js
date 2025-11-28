d5703aa2182e96524b51d5644176660d5f4c074c
星盟 <2785300881@qq.com>
2025-11-28 22:02:16 +0800

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,6 +1,8 @@
 // supabase-config.js
 const SUPABASE_URL = 'https://sgwbztuizxowiacpwzmy.supabase.co';
-const SUPABASE_ANON_KEY = '.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3enBlc3Nhc3N2bW5uYnJuZWdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI0MjU0MTQsImV4cCI6MjA0ODAwMTQxNH0.8lRwB_XmU0b0zPwzu1VqgEwV5qg0g5q5q5q5q5q5q5';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNnd2J6dHVpenhvd2lhY3B3em15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzMzIyMDYsImV4cCI6MjA3OTkwODIwNn0.js6fUBJ9FGRMkVgp80Q-8D6hX-xXbr29rWKyJgOA9b4';
 
 // 创建 Supabase 客户端
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+console.log('Supabase 配置加载完成');
